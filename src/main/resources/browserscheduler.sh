cd2158d9f2a181101e477d0ab8c561103e473fa2
Kimberly <mrkid863@gmail.com>
2023-02-24 09:36:51 +0800

    bump dependency: spring-boot to 3.0.3, selenium to 4.8.1, auto-browser to 2.0.5

--- src/main/resources/browserscheduler.sh
+++ src/main/resources/browserscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dspring.profiles.active=jsonlog -Dchrome_sandbox=no -cp "lib/:lib/*:ext/:ext/*" org.kquiet.browserscheduler.Launcher
+exec java -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=jsonlog -Dotel.exporter.otlp.endpoint=${otlp_endpoint:-http://localhost:4317} -Dotel.resource.attributes=service.name=${project.artifactId},service.version=${project.version} -Dchrome_sandbox=no -cp "lib/:lib/*:ext/:ext/*" org.kquiet.browserscheduler.Launcher
