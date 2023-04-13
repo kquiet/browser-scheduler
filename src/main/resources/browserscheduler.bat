d6cf4551d2674a3179ad2c5a5a3d8459278f32fd
Kimberly <mrkid863@gmail.com>
2023-04-13 10:32:51 +0800

    bump auto-browser to 2.0.6, spring-boot to 3.0.5

--- src/main/resources/browserscheduler.bat
+++ src/main/resources/browserscheduler.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-start "" javaw -Dspring.profiles.active=jsonlog -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/;lib/*;ext/;ext/*" org.kquiet.browserscheduler.Launcher
+start "" javaw -Dspring.profiles.active=jsonlog -Dchrome_option_args="--no-sandbox,--disable-dev-shm-usage" -Dwebdriver_headless=yes -cp "lib/;lib/*;ext/;ext/*" org.kquiet.browserscheduler.Launcher
