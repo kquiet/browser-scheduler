dc739ce6757a19b17f268dc1fe96730e63bec27d
Kimberly <mrkid863@gmail.com>
2024-05-03 18:42:01 +0800

    bump auto-browser to 2.0.7 and spring boot to 3.2.5

--- src/main/resources/browserscheduler.sh
+++ src/main/resources/browserscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dspring.profiles.active=jsonlog -Dchrome_option_args="--no-sandbox,--disable-dev-shm-usage" -cp "lib/:lib/*:ext/:ext/*" org.kquiet.browserscheduler.Launcher
+exec java -Dspring.profiles.active=jsonlog -Dfirefox_option_args="--width=1024;--height=768" -Dchrome_option_args="--no-sandbox;--disable-dev-shm-usage;--remote-allow-origins=*;--window-size=1024,768" -cp "lib/:lib/*:ext/:ext/*" org.kquiet.browserscheduler.Launcher
